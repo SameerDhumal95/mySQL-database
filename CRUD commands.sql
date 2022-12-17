8f4151f258e4c96a64d0a28ffc771b0d47c0f37f
Sameer <106860465+SameerDhumal95@users.noreply.github.com>
2022-12-17 12:11:38 +0530

    Add files via upload

---
+++ CRUD commands.sql
@@ -0,0 +1,63 @@
+
+/*Alter Queries*/
+
+/*modifying data types*/
+alter table student modify sName varchar(255);
+
+/*renaming column name*/
+alter table student rename column sName to name;
+
+/*adding keys on existing table*/
+alter table dept add primary key(deptid);
+
+alter table employee add foreign key(deptid) references dept(deptid);
+
+/*adding a column in existing table*/
+alter table dept add sal int after deptid;
+
+/*drop column fron existing table*/
+alter table dept drop sal;
+
+
+
+
+/*Create Queries*/
+
+/*create database*/
+create database db1;
+
+/*create table*/
+create table division(div int primary key, schoolName varchar(25));
+
+
+
+
+/*Insert Queries*/
+insert into division(div,schoolName) values(1,"r.z.p");
+
+
+
+
+/*Drop table*/
+drop division;
+
+
+
+
+/*Truncate-Empty the table*/
+truncate division;
+
+
+
+/*Delete record from table*/
+delete from emp where id = 1009;
+
+
+
+/*Update record*/
+update emp set sal = 50000 where id = 1;
+
+
+
+/*where claues is for filter*/
+select * from emp where id=1;
