9a2faff6b1ada6ddabaacb74072da0b53dfda84e
Sameer <106860465+SameerDhumal95@users.noreply.github.com>
2022-12-17 17:09:49 +0530

    Update CRUD commands.sql

--- CRUD commands.sql
+++ CRUD commands.sql
@@ -61,3 +61,32 @@ update emp set sal = 50000 where id = 1;
 
 /*where claues is for filter*/
 select * from emp where id=1;
+
+
+/*Operators*/
+/*AND*/
+
+/*OR*/
+
+
+/*IN*/
+
+
+/*LIKE*/
+/*starts with A and at least 5 charactor in length*/
+select * from emp where ename like "a____%";
+
+/*starts with A */
+select * from emp where ename like "a%";
+
+/*end with A */
+select * from emp where ename like "%a";
+
+/*start with S and end with A*/
+select * from emp where ename like "s%a";
+
+/*second charactor start with A*/
+select * from emp where ename like "_a%";
+
+/*length is 5 charactor in length*/
+select * from emp where ename like "______%";
